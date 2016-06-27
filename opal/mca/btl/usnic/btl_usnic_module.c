62fb282feb0735061e8b1ed767079a76843169a4
Thananon Patinyasakdikul <apatinya@cisco.com>
2016-06-27 08:55:17 -0700

    Add MCA param to choose libfabric provider.

--- opal/mca/btl/usnic/btl_usnic_module.c
+++ opal/mca/btl/usnic/btl_usnic_module.c
@@ -1328,8 +1328,8 @@ static int usnic_reg_mr(void* reg_data, void* base, size_t size,
     opal_btl_usnic_module_t* mod = (opal_btl_usnic_module_t*)reg_data;
     opal_btl_usnic_reg_t* ur = (opal_btl_usnic_reg_t*)reg;
     int rc;
-
-    rc = fi_mr_reg(mod->domain, base, size, 0, 0, 0, 0, &ur->ur_mr, NULL);
+    static int id = 0;
+    rc = fi_mr_reg(mod->domain, base, size, 0, 0, ++id, 0, &ur->ur_mr, NULL);
     if (0 != rc) {
         return OPAL_ERR_OUT_OF_RESOURCE;
     }
@@ -1728,6 +1728,7 @@ static int init_one_channel(opal_btl_usnic_module_t *module,
     /* Set up the endpoint for this channel */
     rc = create_ep(module, channel);
     if (OPAL_SUCCESS != rc) {
+	    assert(0);
         goto error;
     }
 
@@ -1748,6 +1749,7 @@ static int init_one_channel(opal_btl_usnic_module_t *module,
      * line size so that each segment is guaranteed to start on a
      * cache line boundary.
      */
+    if(max_msg_size > 4000000) max_msg_size = MAX_EP_MSG_SIZE;
     segsize = (max_msg_size + channel->info->ep_attr->msg_prefix_size +
             opal_cache_line_size - 1) & ~(opal_cache_line_size - 1);
     OBJ_CONSTRUCT(&channel->recv_segs, opal_free_list_t);
@@ -1771,7 +1773,7 @@ static int init_one_channel(opal_btl_usnic_module_t *module,
                                         otherwise ctx gets
                                         clobbered */
     if (OPAL_SUCCESS != rc) {
-        goto error;
+	    assert(0);
     }
 
     /* Post receive descriptors */
@@ -1855,11 +1857,14 @@ static void init_local_modex_part1(opal_btl_usnic_module_t *module)
     struct fi_usnic_info *uip = &module->usnic_info;
     struct sockaddr_in *sin;
 
+    if(info->ep_attr->max_msg_size > 4000000) info->ep_attr->max_msg_size = MAX_EP_MSG_SIZE;
+
     sin = info->src_addr;
     modex->ipv4_addr =       sin->sin_addr.s_addr;
     modex->netmask =         uip->ui.v1.ui_netmask_be;
     modex->max_msg_size =    info->ep_attr->max_msg_size;
     modex->link_speed_mbps = uip->ui.v1.ui_link_speed;
+
 
     opal_btl_usnic_snprintf_ipv4_addr(module->if_ipv4_addr_str,
                                       sizeof(module->if_ipv4_addr_str),
@@ -2144,10 +2149,14 @@ static int init_channels(opal_btl_usnic_module_t *module)
         goto destroy;
     }
 
-    rc = fi_open_ops(&module->av->fid, FI_USNIC_AV_OPS_1, 0,
-            (void **)&module->usnic_av_ops, NULL);
-    if (rc != OPAL_SUCCESS) {
-        goto destroy;
+    if(mca_btl_usnic_component.libfabric_use_usnic) {
+
+        rc = fi_open_ops(&module->av->fid, FI_USNIC_AV_OPS_1, 0,
+                (void **)&module->usnic_av_ops, NULL);
+        if (rc != OPAL_SUCCESS) {
+            goto destroy;
+        }
+
     }
 
     memset(&eq_attr, 0, sizeof(eq_attr));
@@ -2186,7 +2195,7 @@ static int init_channels(opal_btl_usnic_module_t *module)
     }
     rc = init_one_channel(module,
             USNIC_DATA_CHANNEL,
-            MAX_EP_MSG_SIZE,
+            module->fabric_info->ep_attr->max_msg_size,
             module->rd_num, module->sd_num, module->cq_num);
     if (rc != OPAL_SUCCESS) {
         goto destroy;
